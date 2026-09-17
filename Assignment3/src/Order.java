efd6e19681284d39cbf3287cefe67656c4061ec7
Garima Gupta <garimagupta200603@gmail.com>
2026-09-17 16:28:36 +0530

    Day3 assignment done

---
+++ Assignment3/src/Order.java
@@ -0,0 +1,42 @@
+﻿import java.util.HashSet;
+import java.util.Set;
+
+public class Order {
+
+    private final int id;
+    private final Set<Integer> productIds;
+
+    public Order(int id) {
+        this.id = id;
+        this.productIds = new HashSet<>();
+    }
+
+    public Order(int id, Set<Integer> productIds) {
+        this.id = id;
+        this.productIds = new HashSet<>(productIds);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Set<Integer> getProductIds() {
+        return Set.copyOf(productIds);
+    }
+
+    public void addProduct(int productId) {
+        productIds.add(productId);
+    }
+
+    public void removeProduct(int productId) {
+        productIds.remove(productId);
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "id=" + id +
+                ", productIds=" + productIds +
+                '}';
+    }
+}
