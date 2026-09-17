79f5aee2792e5239fc1c8bfaee92e34cee349e5f
Garima Gupta <garimagupta200603@gmail.com>
2026-09-17 20:47:18 +0530

    Day3 assignment done

---
+++ Assignment3/src/OrderRepository.java
@@ -0,0 +1,38 @@
+﻿import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class OrderRepository {
+
+    private final Map<Integer, Order> orders = new HashMap<>();
+
+    public Order findById(int id) {
+        return orders.get(id);
+    }
+
+    public List<Order> findAll() {
+        return new ArrayList<>(orders.values());
+    }
+
+    public void save(Order order) {
+
+        if (orders.containsKey(order.getId())) {
+            System.out.println("Order with ID " + order.getId() + " already exists");
+        }
+
+        if (order != null) {
+            orders.put(order.getId(), order);
+        }
+    }
+
+    public void update(Order order) {
+        if (order != null && orders.containsKey(order.getId())) {
+            orders.put(order.getId(), order);
+        }
+    }
+
+    public void delete(int id) {
+        orders.remove(id);
+    }
+}
---
+++ Assignment3/src/ProductRepository.java
@@ -0,0 +1,42 @@
+﻿import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ProductRepository {
+
+    private final Map<Integer, Product> products = new HashMap<>();
+
+    public Product findById(int id) {
+        return products.get(id);
+    }
+
+    public List<Product> findAll() {
+        return new ArrayList<>(products.values());
+    }
+
+    public void save(Product product) {
+
+        if (product == null) {
+            return;
+        }
+
+        if (products.containsKey(product.getId())) {
+            System.out.println("Product with ID " + product.getId() + " already exists");
+            return;
+        }
+
+        products.put(product.getId(), product);
+
+    }
+
+    public void update(Product product) {
+        if (product != null && products.containsKey(product.getId())) {
+            products.put(product.getId(), product);
+        }
+    }
+
+    public void delete(int id) {
+        products.remove(id);
+    }
+}
