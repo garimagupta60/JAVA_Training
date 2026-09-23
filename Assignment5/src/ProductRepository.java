a396d6cbec2fd278f604e904c6735a6ed9957a40
Garima Gupta <garimagupta200603@gmail.com>
2026-09-23 16:49:36 +0530

    Implemented base product and order class

---
+++ Assignment5/src/ProductRepository.java
@@ -0,0 +1,58 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ProductRepository {
+
+    private final Map<Integer, Product> products = new HashMap<>();
+
+    public Product findById(Integer id) {
+        if (id == null) {
+            return null;
+        }
+        return products.get(id);
+    }
+
+    public List<Product> findAll() {
+        return new ArrayList<>(products.values());
+    }
+
+    public List<Product> findAllSorted(Comparator<Product> comparator) {
+
+        List<Product> sortedProducts =
+                new ArrayList<>(products.values());
+
+        sortedProducts.sort(comparator);
+
+        return sortedProducts;
+    }
+
+    public void save(Product product) {
+
+        if (product == null || product.getId() == null) {
+            return;
+        }
+
+        if (products.containsKey(product.getId())) {
+            System.out.println("Product with ID " + product.getId() + " already exists");
+            return;
+        }
+
+        products.put(product.getId(), product);
+
+    }
+
+    public void update(Product product) {
+        if (product != null && product.getId() != null && products.containsKey(product.getId())) {
+            products.put(product.getId(), product);
+        }
+    }
+
+    public void delete(Integer id) {
+        if (id != null) {
+            products.remove(id);
+        }
+    }
+}
